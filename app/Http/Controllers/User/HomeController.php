85d5c36495f4fd6f37dca4e495301e0a812259ee
BachLong <bachthanhlong.it@gmail.com>
2020-03-25 06:51:17 +0700

    query

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -4,16 +4,26 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Model\user\category;
+use App\Model\user\category_post;
 use App\Model\user\post;
 use App\Model\user\tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index(category $category){
         $posts = post::where('status',1)->orderBy('created_at','DESC')->paginate(6);
-        $postCate = $category->posts();
-         return view('user.blog', compact('posts','postCate'));
+//        $postCate = $category->posts();
+
+        $postCate = DB::table('category_posts')
+            ->join('categories', 'category_posts.category_id', '=', 'categories.id')
+            ->join('posts', 'category_posts.post_id', '=', 'posts.id')
+            ->where('categories.slug', '=', 'doi-song')
+            ->take(1)
+            ->get();
+
+        return view('user.blog', compact('posts','postCate'));
     }
 
     public function tag(tag $tag){
